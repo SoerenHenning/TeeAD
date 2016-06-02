f812523b4aafd5278e5df51061b9b5989b8a13f8
Sören Henning <stu114708@informatik.uni-kiel.de>
2016-06-02 11:24:54 +0200

    implemented first version of WeightedForecaster

---
+++ forecast/WeightedForecaster.java
@@ -0,0 +1,48 @@
+package anomalydetection.forecast;
+
+import anomalydetection.TimeSeries;
+import anomalydetection.TimeSeriesPoint;
+
+public class WeightedForecaster implements Forecaster {
+
+	public enum WeightMethod {
+		LOGARITHMIC, LINEAR, EXPONENTIAL
+	}
+
+	private final WeightMethod weightMethod;
+
+	public WeightedForecaster(final WeightMethod weightMethod) {
+		this.weightMethod = weightMethod;
+	}
+
+	@Override
+	public double forecast(final TimeSeries timeSeries) {
+		double weightedSum = 0;
+		double totalWeights = 0;
+
+		// more recent entry means more weight
+		int position = 0; // TODO Start with 0 or 1? (log(0) -> ?)
+		for (TimeSeriesPoint point : timeSeries.getTimeSeriesPoints()) {
+			final double weight = getWeight(position);
+			totalWeights += weight;
+			weightedSum += point.getValue() * weight;
+			position++;
+		}
+
+		return weightedSum / totalWeights;
+	}
+
+	private double getWeight(final int position) {
+		switch (this.weightMethod) {
+		case LOGARITHMIC:
+			return Math.log(position);
+		case LINEAR:
+			return position;
+		case EXPONENTIAL:
+			return Math.exp(position);
+		default:
+			return position;
+		}
+	}
+
+}
