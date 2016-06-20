65e976363a383bdaf8b55a19ca3dd20f7e9983f2
Sören Henning <stu114708@informatik.uni-kiel.de>
2016-06-20 11:30:04 +0200

    added BetterJSONExporter to use Jackson JSON library like Spring Boot

---
+++ BetterJSONExporter.java
@@ -0,0 +1,55 @@
+package anomalydetection;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonEncoding;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+
+import anomalydetection.measurement.AnomalyScoredMeasurement;
+import teetime.framework.AbstractConsumerStage;
+
+public class BetterJSONExporter extends AbstractConsumerStage<AnomalyScoredMeasurement> {
+
+	private final JsonGenerator jsonGenerator;
+
+	public BetterJSONExporter(final File file) {
+
+		try {
+			JsonFactory jsonFactory = new JsonFactory();
+			this.jsonGenerator = jsonFactory.createGenerator(file, JsonEncoding.UTF8);
+			this.jsonGenerator.writeStartArray();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new IllegalStateException(e);
+		}
+
+	}
+
+	@Override
+	protected void execute(final AnomalyScoredMeasurement measurement) {
+		try {
+			this.jsonGenerator.writeStartObject();
+			this.jsonGenerator.writeNumberField("time", measurement.getTime().toEpochMilli());
+			this.jsonGenerator.writeNumberField("measurement", measurement.getValue());
+			this.jsonGenerator.writeNumberField("prediction", measurement.getPrediction());
+			this.jsonGenerator.writeNumberField("anomalyscore", measurement.getAnomalyScore());
+			this.jsonGenerator.writeEndObject();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new IllegalStateException(e);
+		}
+	}
+
+	@Override
+	public void onTerminating() throws Exception {
+		jsonGenerator.writeEndArray();
+		jsonGenerator.close();
+
+		super.onTerminating();
+	}
+
+}
