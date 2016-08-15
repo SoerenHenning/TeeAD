a14e8213f542abcf08df03427b38f1d6eb4c0a33
Sören Henning <stu114708@informatik.uni-kiel.de>
2016-08-15 09:41:28 +0200

    set initial forecaster configurations capacity

--- src/main/java/teead/forecast/ForecasterConfiguration.java
+++ src/main/java/teead/forecast/ForecasterConfiguration.java
@@ -4,6 +4,12 @@ import java.util.HashMap;
 
 public class ForecasterConfiguration extends HashMap<String, String> {
 
+	private static final int INITIAL_MAP_CAPACITY = 1; // Most times the configuration is empty
+
+	public ForecasterConfiguration() {
+		super(INITIAL_MAP_CAPACITY);
+	}
+
 	private static final long serialVersionUID = 1L;
 
 }
